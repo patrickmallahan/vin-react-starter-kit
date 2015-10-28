30d689478a6bae89784748c92c28603504c07bf5
Cory House <housecor@gmail.com>
2015-10-28 16:22:39 -0500

    Strealined render call

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import FuelSavingsCalculator from './components/FuelSavingsCalculator';
 import React from 'react';
-import ReactDom from 'react-dom'; 
+import {render} from 'react-dom'; 
 
 if (typeof document !== 'undefined') { //necessary to keep Mocha from bombing
-	ReactDom.render(<FuelSavingsCalculator />, document.getElementById('app'));
+	render(<FuelSavingsCalculator />, document.getElementById('app'));
 }
