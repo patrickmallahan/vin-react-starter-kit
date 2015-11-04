ee8c026e94ecb84a6aa29fda0ac0168496b34304
Cory House <housecor@gmail.com>
2015-11-04 12:34:00 -0600

    Converted to stateless functional component

--- src/components/FuelSavingsResults.js
+++ src/components/FuelSavingsResults.js
@@ -1,42 +1,46 @@
 import React from 'react';
 
-class FuelSavingsResults extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        let savingsExist = this.props.savings.monthly.replace('$', '') > 0;
-        let savingsClass = savingsExist ? 'savings' : 'loss';
-        let resultLabel = savingsExist ? 'Savings' : 'Loss';
+//This is a stateless functional component. (Also known as pure or dumb component)
+//More info: https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html#stateless-functional-components
+//And https://medium.com/@joshblack/stateless-components-in-react-0-14-f9798f8b992d
+//And starter kit with more examples here: https://github.com/ericelliott/react-pure-component-starter
+let FuelSavingsResults = (props) => {
+    let savingsExist = props.savings.monthly.replace('$', '') > 0;
+    let savingsClass = savingsExist ? 'savings' : 'loss';
+    let resultLabel = savingsExist ? 'Savings' : 'Loss';
 
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <td className="fuel-savings-label">{resultLabel}</td>
-                        <td>
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td>Monthly</td>
-                                        <td>1 Year</td>
-                                        <td>3 Year</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={savingsClass}>{this.props.savings.monthly}</td>
-                                        <td className={savingsClass}>{this.props.savings.annual}</td>
-                                        <td className={savingsClass}>{this.props.savings.threeYear}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
-}
+    //Note that you can even exclude the return statement below if you entire component is
+    //composed within the parenthesies. Here return is necessary because some variables
+    //are set above.
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td className="fuel-savings-label">{resultLabel}</td>
+                    <td>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Monthly</td>
+                                    <td>1 Year</td>
+                                    <td>3 Year</td>
+                                </tr>
+                                <tr>
+                                    <td className={savingsClass}>{props.savings.monthly}</td>
+                                    <td className={savingsClass}>{props.savings.annual}</td>
+                                    <td className={savingsClass}>{props.savings.threeYear}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
 
+//Note that this odd style is utilized for propType validation for now. Must be defined *after* 
+//the component is defined, which is why it's separate and down here.
 FuelSavingsResults.propTypes = {
     savings: React.PropTypes.object.isRequired
 };
