0a75bd45269e1ba5048367dafec3846a9ecc2386
Cory House <housecor@gmail.com>
2015-11-11 13:14:20 -0600

    Added comment

--- src/components/FuelSavingsResults.spec.js
+++ src/components/FuelSavingsResults.spec.js
@@ -6,6 +6,16 @@ import ReactDOMServer from 'react/lib/ReactDOMServer';
 
 chai.should();
 
+/*This test file displays how to test a React component's HTML
+  outside of the browser. It uses Cheerio, which is a handy
+  server-side library that mimics jQuery. So to test a React
+  components HTML for a given state we do the following:
+  1. Instantiate the component and pass the desired prop values
+  2. Use ReactDOMServer to generate the resulting HTML
+  3. Use Cheerio to load the HTML into a fake DOM
+  4. Use Cheerio to query the DOM using jQuery style selectors
+  5. Assert that certain DOM elements exist with expected values.
+ */
 describe('Fuel Savings Calculator Results Component', () => {
 	describe('Savings label', () => {
 		it('displays as savings when savings exist', () => {
