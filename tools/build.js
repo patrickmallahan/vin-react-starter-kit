635d14fe2a6ce8ae7a0adffc54cc0215fc773dfc
Cory House <housecor@gmail.com>
2015-12-29 21:47:32 -0600

    Augmented webpack build to display more details warnings and errors.

--- tools/build.js
+++ tools/build.js
@@ -1,13 +1,48 @@
+//More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
 var webpack = require('webpack');
 var webpackConfigBuilder = require('../webpack.config');
 var webpackConfig = webpackConfigBuilder('production');
+var colors = require('colors');
+var argv = require('yargs').argv;
+var inSilentMode = argv.s; //set to true when -s is passed on the command line
 
-console.log('Generating minified bundle for production use via Webpack...');
+if (!inSilentMode) {
+  console.log('Generating minified bundle for production use via Webpack...'.bold.blue);
+}
+
+var handleFatalError = function(error) {
+  console.log(error.bold.red);
+  return 1;
+}
+
+var handleWarnings = function(warnings) {
+  console.log('Webpack generated the following warnings: '.bold.yellow);
+  warnings.map(warning => console.log(warning.yellow));
+};
+
+var handleSoftErrors = function(errors) {
+  errors.map(errors => console.log(error.red));
+};
 
 webpack(webpackConfig).run((err, stats) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log('Bundle generated at ' + webpackConfig.output.path);
+    return handleFatalError(err);
   }
+
+  var jsonStats = stats.toJson();
+
+  if (jsonStats.errors.length > 0) {
+    return handleSoftErrors(jsonStats.errors);
+  }
+
+  if (jsonStats.warnings.length > 0) {
+    //For now, not displaying warnings. Seems like it's all noise.
+    if (!inSilentMode) { //suppress warnings in silent mode.
+      handleWarnings(jsonStats.warnings);
+    }
+  }
+
+  //if we got this far, the build succeeded.
+  console.log('Your app has been compiled in production mode and written to /dist. It\'s now ready for commit.'.green.bold);
+  return 0;
 });
