d05f09af4e7631d3a5ef6cfc94b2e932ea2dac0d
Cory House <housecor@gmail.com>
2015-12-31 11:37:04 -0600

    Bux fix: Devserver now using new webpack.config builder

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var browserSync = require('browser-sync');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
+var webpackConfigBuilder = require('./webpack.config');
+var webpackConfig = webpackConfigBuilder('development');
 
-// Require ./webpack.config.js and make a bundler from it
-var webpackConfig = require('./webpack.config');
 var bundler = webpack(webpackConfig);
 
 // Run Browsersync and use middleware for Hot Module Replacement
