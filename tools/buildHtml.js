daaa752f161e7d6a5a4e432091248a63e80d38c1
Cory House <housecor@gmail.com>
2016-01-02 09:42:01 -0600

    Streamlined HTML build

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -2,16 +2,18 @@
 // and replaces the trackJs placeholder with the actual trackJS code for use in production.
 var fs = require('fs');
 var colors = require('colors');
+var cheerio = require('cheerio');
 
 fs.readFile('src/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err.bold.red);
   }
 
-  var trackJsCode = "<!-- BEGIN TRACKJS Note: This should be the first <script> on the page per https://my.trackjs.com/install --><script>window._trackJs = { token: '69ff2d61429a4c5da881a2026decd7d7' };</script><script src=https://d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js></script><!-- END TRACKJS -->";
-  var result = data.replace(/<!--DO NOT REMOVE. KEEP AT TOP OF HEAD. TrackJS is inserted here during prod build-->/g, trackJsCode);
+  var trackJsCode = "<script>window._trackJs = { token: '69ff2d61429a4c5da881a2026decd7d7' };</script><script src=https://d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js></script>";
+  var $ = cheerio.load(data);
+  $('head').prepend(trackJsCode); //add TrackJS tracking code to the top of <head> as suggested in their docs.
 
-  fs.writeFile('dist/index.html', result, 'utf8', function (err) {
+  fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
     if (err) return console.log(err.bold.red);
   });
 
