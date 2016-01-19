0600167ad660bd3fc80ea9de988803c73d1595c3
Cory House <cory.house@vinsolutions.com>
2016-01-19 15:06:52 -0600

    Removing coloring of error messages since it made Windows cranky

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -6,7 +6,7 @@ var cheerio = require('cheerio');
 
 fs.readFile('src/index.html', 'utf8', function (err,data) {
   if (err) {
-    return console.log(err.bold.red);
+    return console.log(err);
   }
 
   var trackJsCode = "<script>window._trackJs = { token: '69ff2d61429a4c5da881a2026decd7d7' };</script><script src=https://d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js></script>";
@@ -14,7 +14,7 @@ fs.readFile('src/index.html', 'utf8', function (err,data) {
   $('head').prepend(trackJsCode); //add TrackJS tracking code to the top of <head> as suggested in their docs.
 
   fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
-    if (err) return console.log(err.bold.red);
+    if (err) return console.log(err);
   });
 
   console.log('index.html written to /dist'.green);
