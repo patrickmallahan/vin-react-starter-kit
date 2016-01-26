73fa0736834147be9b6e1080880836f975e1cf5b
Cory House <housecor@gmail.com>
2016-01-26 11:23:11 -0600

    Temporarily removing api example to resolve bug

--- src/components/FuelSavingsApp.js
+++ src/components/FuelSavingsApp.js
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react';
 import FuelSavingsResults from './FuelSavingsResults';
 import FuelSavingsTextInput from './FuelSavingsTextInput';
-import api from '../api/api';
 
 const FuelSavingsApp = (props) => {
   const save = function () {
@@ -14,12 +13,6 @@ const FuelSavingsApp = (props) => {
 
   const fuelSavingsKeypress = function (name, value) {
     props.actions.calculateFuelSavings(props, name, value);
-  };
-
-  const getCustomerAttachments = function() {
-    api.getCustomerAttachments(212746634).then(function(response) {
-      alert('Response status: ' + response.status);
-    });
   };
 
   const settings = props.fuelSavingsAppState;
@@ -66,7 +59,7 @@ const FuelSavingsApp = (props) => {
         <hr/>
 
         {settings.necessaryDataIsProvidedToCalculateSavings ? <FuelSavingsResults savings={settings.savings} /> : null}
-        <input type="submit" value="Save" onClick={this.save} /> <a href="#" onClick={this.getCustomerAttachments}>Make AJAX call</a>
+        <input type="submit" value="Save" onClick={save} />
       </div>
   );
 };
