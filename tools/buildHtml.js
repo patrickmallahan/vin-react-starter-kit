17453b0d22fe38b45bc87091e9e037e789a5e139
Cory House <housecor@gmail.com>
2016-01-28 14:10:24 -0600

    Updated comment

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,7 +1,12 @@
 // This script copies src/index.html into /dist/index.html
-// and replaces the trackJs placeholder with the actual trackJS code for use in production.
+// and adds TrackJS error tracking code for use in production
+// when useTrackJs is set to true below and a trackJsToken is provided.
+// This is a good example of using Node and cheerio to do a simple file transformation.
+// In this case, the transformation is useful since we only want to track errors in the built production code.
+
 // Allowing console calls below since this is a build file.
 /*eslint-disable no-console */
+
 var fs = require('fs');
 var colors = require('colors');
 var cheerio = require('cheerio');
